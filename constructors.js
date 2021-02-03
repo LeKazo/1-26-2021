1ff695562654c75d476a9b90843bbe63f58374e8
LeKazo <70152390+LeKazo@users.noreply.github.com>
2021-02-03 07:52:54 -0500

    contructor

---
+++ constructors.js
@@ -0,0 +1,10 @@
+function Dog() {
+    this.name = "Mozzy";
+    this.color = "White";
+    this.numLegs = 4;
+}
+
+let whiteDog = new Dog();
+dog.name = "Quez";
+dog.color = "black";
+dog.legs = 4;
